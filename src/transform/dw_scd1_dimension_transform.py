affa2e2686fb630d8c392a4109ee497da1f5469d
Raj Phadke <phadke.raj10@gmail.com>
2025-05-26 22:51:41 -0400

    Creating SCD Type1 Dimension

--- src/transform/dw_scd1_dimension_transform.py
+++ src/transform/dw_scd1_dimension_transform.py
@@ -8,11 +8,11 @@ class DwSCDType1DimensionTransform(BaseDwDimensionTransform):
     Concreate implementation to create Dw SCD Type 1 Dimension
     """
 
-    def add_metadata_columns(df: DataFrame) -> DataFrame:
+    def add_metadata_columns(self, df: DataFrame) -> DataFrame:
         """
         Method to add SCD Type1 specific metadata columns
         """
-
+        self.logger.info("Adding metadata columns to the df")
         return df.withColumn("dw_created_at", lit(current_timestamp()))
 
     def create_dimension_data(self) -> DwEntity:
@@ -23,11 +23,12 @@ class DwSCDType1DimensionTransform(BaseDwDimensionTransform):
         input_df = self.utils.lowercase_all_columns(df=self.config.base_data.df)
 
         # Select required columns
-        df_selected = self.utils.select_columns(
-            df=input_df, cols=self.config.base_data.select_columns
-        )
+        select_columns = self.config.base_data.select_columns
+        self.logger.info(f"Selecting specified columns: {select_columns}")
+        df_selected = self.utils.select_columns(df=input_df, cols=select_columns)
 
         # Apply column mapping and casting
+        self.logger.info("Applying column mapping")
         df_remapped = self.utils.rename_and_cast_columns_with_check(
             df=df_selected, rename_map=self.config.base_data.column_mapping
         )
@@ -35,6 +36,7 @@ class DwSCDType1DimensionTransform(BaseDwDimensionTransform):
         sk_column_name = f"sk_{self.entity_name.lower()}"
 
         # Create sk column using hash
+        self.logger.info(f"Creating Dimension SK: {sk_column_name}")
         df_with_hash_column = self.utils.create_hash_column(
             df=df_remapped,
             col_list=self.config.primary_key_columns,
@@ -42,6 +44,7 @@ class DwSCDType1DimensionTransform(BaseDwDimensionTransform):
         )
 
         # Rearrange the df
+        self.logger.info("Rearranging df columns")
         df_rearranged = self.utils.rearrange_columns(
             df=df_with_hash_column, col_name=sk_column_name
         )
